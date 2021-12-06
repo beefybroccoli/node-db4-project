8d79bba5a417ce8ae2181423a73429634961d34e
beefybroccoli <tedkimemail@yahoo.com>
2021-12-05 17:39:55 -0800

    ...

---
+++ database/migrations/20211206013011_01-create-users.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema 
+    .createTable("users", table=>{
+        table.increments("id");
+        table.string("username").notNull();
+        table.string("password").notNull();
+    })
+  
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists("users");
+};
