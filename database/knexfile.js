e76aeff46090f7a0f5827b23e3e9232d1b5b71b9
beefybroccoli <tedkimemail@yahoo.com>
2021-12-05 19:41:26 -0800

    something wrong with db configuration

--- database/knexfile.js
+++ database/knexfile.js
@@ -5,8 +5,15 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './database/dev.sqlite3'
+    },
+    useNullAsDefault: true,
+    migration:{
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    },
   },
 
   staging: {
