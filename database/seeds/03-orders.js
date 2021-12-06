cd6996478ec30f1bd4a0c07f2f9b4fc5d9a7062f
beefybroccoli <tedkimemail@yahoo.com>
2021-12-05 18:33:10 -0800

    ...

--- database/seeds/03-orders.js
+++ database/seeds/03-orders.js
@@ -1,13 +1,15 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('orders').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('orders').insert([
+        {id: 1, product_id:1, quantity:5, status:"pending",user_id:1},
+        {id: 2, product_id:2, quantity:5, status:"pending",user_id:2},
+        {id: 3, product_id:3, quantity:5, status:"pending",user_id:3},
+        {id: 4, product_id:4, quantity:5, status:"pending",user_id:4},
+        {id: 5, product_id:5, quantity:5, status:"pending",user_id:5},
       ]);
     });
 };
--- database/seeds/04-products.js
+++ database/seeds/04-products.js
@@ -1,13 +1,15 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('products').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('products').insert([
+        {id: 1, name:"color shirt",description:"a very colorful shirt", price:11.99},
+        {id: 2, name:"pencil case",description:"a very strong case", price:11.99},
+        {id: 3, name:"water bottle",description:"a long lasting bottle", price:11.99},
+        {id: 4, name:"container box",description:"a wood box", price:11.99},
+        {id: 5, name:"fish tank",description:"this tank can hold 5 fishes", price:11.99},
       ]);
     });
 };
