ac472561d298bf9d3fa2d7a0246b169391ff9030
beefybroccoli <tedkimemail@yahoo.com>
2021-12-06 22:38:46 -0800

    ...

--- api/middleware-verify.js
+++ api/middleware-verify.js
@@ -1,3 +1,5 @@
+//verify input is not null, not undefined and not empty
+//verify input is a type of string
 function verifyString(input){
     if(input === null || input === undefined || typeof input !== "string" || input.trim()===""){
         return false;
@@ -6,6 +8,8 @@ function verifyString(input){
     }
 }
 
+//verify the input is not null, not undefined
+//verify the input is a type of number
 function verifyNumber(input){
     if(input === null || input === undefined || typeof input !== "number"){
         return false;
