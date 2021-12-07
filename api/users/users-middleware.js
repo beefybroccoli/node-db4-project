9570aa9f0385fcb04eef66b2b9887ca4097fac11
beefybroccoli <tedkimemail@yahoo.com>
2021-12-07 00:28:21 -0800

    ...

--- api/users/users-middleware.js
+++ api/users/users-middleware.js
@@ -3,7 +3,7 @@ const {verifyNumber, verifyString, verifyStringLength} = require("../middleware-
 
 async function verify_user_id(req, res, next){
     const {id} = req.params;
-    console.log("typeof id = ", typeof id );
+    // console.log("typeof id = ", typeof id );
     if(verifyNumber(id)){
         res.status(400).json({message:`invalid id - ${id}`});
     }else{
@@ -13,7 +13,7 @@ async function verify_user_id(req, res, next){
             res.status(404).json({message:`id ${id} not found`});
         }else{
             req.user = array[0];
-            console.log("req.user = ", req.user);
+            // console.log("req.user = ", req.user);
             next();
         }
     }
