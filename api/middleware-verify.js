0d8f5126f4adf98e1ae40c0899e8e013977b467f
beefybroccoli <tedkimemail@yahoo.com>
2021-12-08 15:56:23 -0800

    ...

--- api/middleware-verify.js
+++ api/middleware-verify.js
@@ -51,4 +51,4 @@ function verifyOrderStatus(order_status){
     return (order_status === "pending" || order_status === "shipped" || order_status === "delievered");
 }
 
-module.exports = {verifyString, verifyNumber, verifyStringLength, verifyEmptyArray, verifyUserType, verifyOrderStatus};
+module.exports = {verifyString, verifyNumber, verifyStringLength, verifyEmptyArray, verifyUserType, verifyOrderStatus, verifyNull, verifyUndefined};
